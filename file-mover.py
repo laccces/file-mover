6794e0a7d02b20eaf851c91b787b1715805886dd
Dan <en07djc@icloud.com>
2022-09-07 20:02:29 +0100

    Added file types.

---
+++ file-mover.py
@@ -0,0 +1,146 @@
+import glob
+import shutil
+
+# CSV
+source_files='/Users/dancoman/Downloads/*.csv'
+target_folder='/Users/dancoman/Downloads/CSV'
+
+filelist=glob.glob(source_files)
+for single_file in filelist:
+     # move file with full paths as shutil.move() parameters
+    shutil.move(single_file,target_folder) 
+
+# TXT
+source_files='/Users/dancoman/Downloads/*.txt'
+target_folder='/Users/dancoman/Downloads/TXT'
+
+filelist=glob.glob(source_files)
+for single_file in filelist:
+     # move file with full paths as shutil.move() parameters
+    shutil.move(single_file,target_folder) 
+
+# PDF
+source_files='/Users/dancoman/Downloads/*.pdf'
+target_folder='/Users/dancoman/Downloads/PDF'
+
+filelist=glob.glob(source_files)
+for single_file in filelist:
+     # move file with full paths as shutil.move() parameters
+    shutil.move(single_file,target_folder) 
+
+# XLSX
+source_files='/Users/dancoman/Downloads/*.xlsx'
+target_folder='/Users/dancoman/Downloads/xlsx'
+
+filelist=glob.glob(source_files)
+for single_file in filelist:
+     # move file with full paths as shutil.move() parameters
+    shutil.move(single_file,target_folder) 
+
+# DocX
+source_files='/Users/dancoman/Downloads/*.docx'
+target_folder='/Users/dancoman/Downloads/Docs'
+
+filelist=glob.glob(source_files)
+for single_file in filelist:
+     # move file with full paths as shutil.move() parameters
+    shutil.move(single_file,target_folder) 
+
+# xls
+source_files='/Users/dancoman/Downloads/*.xls'
+target_folder='/Users/dancoman/Downloads/Docs'
+
+filelist=glob.glob(source_files)
+for single_file in filelist:
+     # move file with full paths as shutil.move() parameters
+    shutil.move(single_file,target_folder) 
+
+# DMG
+source_files='/Users/dancoman/Downloads/*.dmg'
+target_folder='/Users/dancoman/Downloads/DMGs'
+
+filelist=glob.glob(source_files)
+for single_file in filelist:
+     # move file with full paths as shutil.move() parameters
+    shutil.move(single_file,target_folder) 
+
+# ZIP
+source_files='/Users/dancoman/Downloads/*.zip'
+target_folder='/Users/dancoman/Downloads/ZIP'
+
+filelist=glob.glob(source_files)
+for single_file in filelist:
+     # move file with full paths as shutil.move() parameters
+    shutil.move(single_file,target_folder) 
+
+# PNG
+source_files='/Users/dancoman/Downloads/*.png'
+target_folder='/Users/dancoman/Downloads/Images'
+
+filelist=glob.glob(source_files)
+for single_file in filelist:
+     # move file with full paths as shutil.move() parameters
+    shutil.move(single_file,target_folder) 
+
+# JPG
+source_files='/Users/dancoman/Downloads/*.jpg'
+target_folder='/Users/dancoman/Downloads/Images'
+
+filelist=glob.glob(source_files)
+for single_file in filelist:
+     # move file with full paths as shutil.move() parameters
+    shutil.move(single_file,target_folder) 
+
+# SVG
+source_files='/Users/dancoman/Downloads/*.svg'
+target_folder='/Users/dancoman/Downloads/Images'
+
+filelist=glob.glob(source_files)
+for single_file in filelist:
+     # move file with full paths as shutil.move() parameters
+    shutil.move(single_file,target_folder) 
+
+# HTML
+source_files='/Users/dancoman/Downloads/*.html'
+target_folder='/Users/dancoman/Downloads/HTML'
+
+filelist=glob.glob(source_files)
+for single_file in filelist:
+     # move file with full paths as shutil.move() parameters
+    shutil.move(single_file,target_folder) 
+
+# EML
+source_files='/Users/dancoman/Downloads/*.eml'
+target_folder='/Users/dancoman/Downloads/Emails'
+
+filelist=glob.glob(source_files)
+for single_file in filelist:
+     # move file with full paths as shutil.move() parameters
+    shutil.move(single_file,target_folder) 
+
+# MOV
+source_files='/Users/dancoman/Downloads/*.mov'
+target_folder='/Users/dancoman/Downloads/Videos'
+
+filelist=glob.glob(source_files)
+for single_file in filelist:
+     # move file with full paths as shutil.move() parameters
+    shutil.move(single_file,target_folder) 
+
+# mp4
+source_files='/Users/dancoman/Downloads/*.mp4'
+target_folder='/Users/dancoman/Downloads/Videos'
+
+filelist=glob.glob(source_files)
+for single_file in filelist:
+     # move file with full paths as shutil.move() parameters
+    shutil.move(single_file,target_folder) 
+
+# mp3
+source_files='/Users/dancoman/Downloads/*.mp3'
+target_folder='/Users/dancoman/Downloads/Music'
+
+filelist=glob.glob(source_files)
+for single_file in filelist:
+     # move file with full paths as shutil.move() parameters
+    shutil.move(single_file,target_folder) 
